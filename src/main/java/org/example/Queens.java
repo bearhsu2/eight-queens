cfc8f0f4e731613f21aa8c779ca5bf08b673bee0
Yu-Song Syu <kuma@simbo.com.tw>
2021-01-18 22:34:57 +0800

    make all methods non-static

--- src/main/java/org/example/Queens.java
+++ src/main/java/org/example/Queens.java
@@ -2,17 +2,14 @@ package org.example;
 
 public class Queens {
 
-    public static boolean isLegal(int[] q, int n) {
-        for (int i = 0; i < n; i++) {
-            if (q[i] == q[n]) return false;   // same column
-            if ((q[i] - q[n]) == (n - i)) return false;   // same major diagonal
-            if ((q[n] - q[i]) == (n - i)) return false;   // same minor diagonal
-        }
-        return true;
+
+    public static void main(String[] args) {
+        int[] answer = new int[8];
+        new Queens().enumerate(answer, 0);
     }
 
 
-    public static void print(int[] q) {
+    public void print(int[] q) {
         int n = q.length;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -26,22 +23,25 @@ public class Queens {
     }
 
 
-    public static void enumerate(int[] q, int k) {
+    public void enumerate(int[] q, int k) {
         int n = q.length;
         if (k == n) print(q);
         else {
             for (int i = 0; i < n; i++) {
                 q[k] = i;
-                if (isLegal(q, k)) enumerate(q, k + 1);
+                if (new Queens().isLegal(q, k)) new Queens().enumerate(q, k + 1);
             }
         }
     }
 
 
-    public static void main(String[] args) {
-        int[] answer = new int[8];
-        enumerate(answer, 0);
+    public boolean isLegal(int[] q, int n) {
+        for (int i = 0; i < n; i++) {
+            if (q[i] == q[n]) return false;   // same column
+            if ((q[i] - q[n]) == (n - i)) return false;   // same major diagonal
+            if ((q[n] - q[i]) == (n - i)) return false;   // same minor diagonal
+        }
+        return true;
     }
-
 }
 
